82d0c5b3a6a6b255109f35a5ba3c26d0a50c86e5
Shyam Padia <exmachina@Shyams-MacBook-Pro.local>
2017-12-22 22:20:29 -0500

    send email added

---
+++ send_email/email.py
@@ -0,0 +1,19 @@
+import smtplib
+
+content = 'Example email stuff here'
+
+mail = smtplib.SMTP('smtp.gmail.com', port=587)
+
+mail.ehlo()
+
+mail.starttls()
+
+# mail.login('email', 'password')
+mail.login('shyam.padia930@gmail.com', 'I@minpeace3')
+
+# mail.sendmail('fromemail', 'receiver', content)
+mail.sendmail('padia.shyam@awesome.com', 'shyam.padia@gmail.com', content)
+
+
+mail.close()
+

2146fcee6b62b19bba0d76a839f5811d055fc053
exMachina <shyampadia@live.com>
2017-12-31 17:44:48 -0500

    email changed

--- send_email/email.py
+++ send_email/email.py
@@ -9,7 +9,7 @@ mail.ehlo()
 mail.starttls()
 
 # mail.login('email', 'password')
-mail.login('shyam.padia930@gmail.com', 'I@minpeace3')
+mail.login('shyam.padia930@gmail.com', 'xxx')
 
 # mail.sendmail('fromemail', 'receiver', content)
 mail.sendmail('padia.shyam@awesome.com', 'shyam.padia@gmail.com', content)
